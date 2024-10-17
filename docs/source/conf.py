41dd2c196503d790e0510e6ec55d6d928572a4e3
kjh4443 <n7484443@hanyang.ac.kr>
2024-10-17 15:30:56 +0900

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ html_static_path = ['_static']
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../backbone/DeepFoldPublic'))
-sys.path.insert(0, os.path.abspath('../../backbone/RoseTTAFold2'))
+sys.path.insert(0, os.path.abspath('../../backbone/RoseTTAFold2/SE3Transformer'))
+sys.path.insert(0, os.path.abspath('../../backbone/RoseTTAFold2/network'))
 sys.path.insert(0, os.path.abspath('../../tool/BIS-ProteinStructure-libraries'))
 sys.path.insert(0, os.path.abspath('../../tool/galaxydock_dl/src'))
